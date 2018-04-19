401f9386044808142332c6dcf2b98133dccdad6d
estephanian <estephanian@users.noreply.github.com>
2018-04-19 11:19:34 -0500

    Add files via upload
    API Helper for connecting with API

---
+++ HymnDBFinal/HymnDBFinal/ApiHelper.swift
@@ -0,0 +1,27 @@
+//
+//  ApiHelper.swift
+//  HymnDBFinal
+//
+//  Created by Emily Stephanian on 4/18/18.
+//  Copyright © 2018 Adrien Ibarra. All rights reserved.
+//
+
+import UIKit
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+let PRODUCTION_ENV = "http://ec2-34-215-244-252.us-west-2.compute.amazonaws.com/"
+
+
+
+class ApiHelper {
+    static var userId = "77"
+    static var AUTH_TOKEN = ""
+
+
+    }
+
+
+}
