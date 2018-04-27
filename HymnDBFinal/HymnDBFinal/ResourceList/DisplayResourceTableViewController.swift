4b3969a1bf920c74a77f7cfbb68f91bfc832d2af
Logan Dorsey <logandorsey@gmail.com>
2018-04-26 22:16:42 -0500

    Now pulling data and displaying a table

--- HymnDBFinal/HymnDBFinal/ResourceList/DisplayResourceTableViewController.swift
+++ HymnDBFinal/HymnDBFinal/ResourceList/DisplayResourceTableViewController.swift
@@ -17,9 +17,12 @@ import AlamofireImage
 class DisplayResourceTableViewController: UITableViewController {
     var resourceList:JSON = []
     
+
     
     override func viewDidLoad() {
         
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
 
         let url = "http://ec2-34-209-251-224.us-west-2.compute.amazonaws.com:3000/all/Book"
         
@@ -27,17 +30,33 @@ class DisplayResourceTableViewController: UITableViewController {
             switch response.result {
             case .success(let value):
                 self.resourceList = JSON(value)
-                print(self.resourceList)
+                self.resourceList = JSON(self.resourceList["resources"])
+                print(self.resourceList.count)
+                self.tableView.reloadData()
             case .failure(let error):
                 print(error)
             }
         }
+        self.tableView.reloadData()
         
-        super.viewDidLoad()
+        super.viewDidLoad();
 
 }
+    
+}
+
+extension DisplayResourceTableViewController {
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as UITableViewCell
+        
+        cell.textLabel?.text = self.resourceList[indexPath.row]["title"].string
+        print(resourceList[indexPath.row]["title"].string)
+        return cell
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return resourceList.count
+        return self.resourceList.count
     }
     
 }
